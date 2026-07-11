7d2376bf2cf435c37541a95104c0fb1685a7bd12
buttercode-dev <buttercoder.dev@gmail.com>
2026-07-11 15:36:51 +0200

    Remove superseded browser console configuration

--- ops/config.js
+++
@@ -1,5 +0,0 @@
-window.TAD_OPS_CONFIG = Object.freeze({
-  supabaseUrl: 'https://pzvytksdpwnsnixcbrzr.supabase.co',
-  publishableKey: 's\u0062_publishable_a-iMxWAVN1OZDywsf-g99A_o8RgS_na',
-  dueTodayUrl: 'https://due-today-six.vercel.app/app'
-});
